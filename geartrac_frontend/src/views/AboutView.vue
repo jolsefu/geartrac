b6a9627108bb04e29cf399a5de5cdd28f8a10f58
jolsefu <jules.leo.reserva@gmail.com>
2025-03-20 19:32:01 +0800

    Added tailwindcss and some design changes

--- geartrac_frontend/src/views/AboutView.vue
+++ geartrac_frontend/src/views/AboutView.vue
@@ -1,15 +1,11 @@
+<script setup>
+  import Info from '../components/Info.vue'
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <Info />
+  <div class="text-large">This is About</div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 </style>
