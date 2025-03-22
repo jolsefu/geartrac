52f7727856536ab80d3184fb3ebe39dae52a5023
jolsefu <jules.leo.reserva@gmail.com>
2025-03-22 19:17:27 +0800

    Added views and urls for sign up

--- geartrac_backend/geartrac_app/urls.py
+++ geartrac_backend/geartrac_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index/", views.index, name="index")
+    path("signup/", views.SignupView.as_view(), name="signup"),
+    path("index/", views.index, name="index"),
 ]
--- geartrac_backend/geartrac_app/views.py
+++ geartrac_backend/geartrac_app/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Response
+from .serializers import UserSerializer
 from django.http import HttpResponse
 
+class SignupView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+
 def index(request):
-    return HttpResponse("Hello, world!")
+    return HttpResponse("Hello, there")
