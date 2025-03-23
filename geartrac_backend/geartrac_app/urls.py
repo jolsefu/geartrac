b6164b55e89e432dd5f832494a2017a282a57a69
jolsefu <jules.leo.reserva@gmail.com>
2025-03-23 08:10:42 +0800

    Removed extra code for google auth

--- geartrac_backend/geartrac_app/urls.py
+++ geartrac_backend/geartrac_app/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("signup/", views.SignupView.as_view(), name="signup"),
     path("index/", views.index, name="index"),
 ]
--- geartrac_backend/geartrac_app/views.py
+++ geartrac_backend/geartrac_app/views.py
@@ -1,14 +1,4 @@
-from rest_framework.views import APIView, Response
-from .serializers import UserSerializer
 from django.http import HttpResponse
-
-class SignupView(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data)
 
 def index(request):
     return HttpResponse("Hello, there")
