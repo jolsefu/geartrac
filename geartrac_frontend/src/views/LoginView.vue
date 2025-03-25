f7d3358d75f459cb1eff51c473b0f428cee30a43
jolsefu <jules.leo.reserva@gmail.com>
2025-03-25 13:22:25 +0800

    Changed LoginView

--- geartrac_frontend/src/views/LoginView.vue
+++ geartrac_frontend/src/views/LoginView.vue
@@ -1,18 +1,30 @@
 <script setup>
 import { ref } from 'vue'
-import Authenticate from '../components/Authenticate.vue'
+import { authLogIn, authLogOut, authTest } from '@/auth'
 
 const isVisible = ref(false)
 
 setTimeout(() => {
   isVisible.value = true
-}, 200);
+}, 200)
 </script>
 
 <template>
   <transition name="swipe-up">
-    <div v-if="isVisible" class="flex items-center h-screen pt-[8rem] flex-col">
+    <!-- <div v-if="isVisible" class="flex items-center h-screen pt-[8rem] flex-col">
       <Authenticate form-title="Login" />
+    </div> -->
+
+    <div class="flex items-center justify-center h-screen pt-[8rem] flex-col">
+      <div>
+        <button @click="authLogIn">Login Using Google</button>
+      </div>
+      <div>
+        <button @click="authLogOut">Logout Using Google</button>
+      </div>
+      <div>
+        <button @click="authTest">Test Authentication</button>
+      </div>
     </div>
   </transition>
  </template>
