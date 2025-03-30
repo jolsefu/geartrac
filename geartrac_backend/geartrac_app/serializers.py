f8096731aaffdf6a2d4c7af0ecbb9e728f9f31b7
jolsefu <jules.leo.reserva@gmail.com>
2025-03-30 22:34:00 +0800

    Changed gear serializer to return first name and last name

--- geartrac_backend/geartrac_app/serializers.py
+++ geartrac_backend/geartrac_app/serializers.py
@@ -9,8 +9,19 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class GearSerializer(serializers.ModelSerializer):
-    owner = serializers.StringRelatedField(allow_null=True, default=None)
+    owner = serializers.SerializerMethodField()
+    used_by = serializers.SerializerMethodField()
 
     class Meta:
         model = Gear
-        fields = ['name', 'unit_description', 'property_number', 'owner', 'used', 'borrowed']
+        fields = ['name', 'used_by', 'unit_description', 'property_number', 'owner', 'used', 'borrowed']
+
+    def get_owner(self, obj):
+        if obj.owner:
+            return f'{obj.owner.first_name} {obj.owner.last_name}'.strip() or obj.owner.username
+        return None
+
+    def get_used_by(self, obj):
+        if hasattr(obj, 'used_by') and obj.used_by:
+            return f'{obj.used_by.first_name} {obj.used_by.last_name}'.strip() or obj.used_by.username
+        return None
