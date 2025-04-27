0c70f2af39c1672eed06f68971bdd531f8143c98
jolsefu <jules.leo.reserva@gmail.com>
2025-04-27 20:28:56 +0800

    Vue transition doesn't seem to work so i just gave up

--- geartrac_frontend/src/components/Notify.vue
+++ geartrac_frontend/src/components/Notify.vue
@@ -6,15 +6,15 @@ const props = defineProps({
   notify: { type: Object, required: true },
 })
 
-const open = ref(true);
+const open = ref(false);
 
 watch(
   props.notify,
   (newNotify) => {
-    if (newNotify.message && newNotify.messageTitle) {
+    if (newNotify.message && newNotify.messageTitle && !open.value) {
       open.value = true;
       setTimeout(() => {
-        isVisible.value = false;
+        open.value = false;
       }, 7000);
     }
   },
@@ -23,23 +23,23 @@ watch(
 </script>
 
 <template>
-  <ToastProvider>
+  <ToastProvider swipe-threshold="50">
     <ToastRoot
-      v-model:open="open"
-      class="bg-white rounded-lg shadow-sm border p-[15px] grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--reka-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
+      :open="open"
+      class="rounded-lg shadow-sm border p-[15px] grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center"
     >
       <ToastTitle class="[grid-area:_title] mb-[5px] font-medium text-slate12 text-sm">
-        HELLO
+        {{ notify.messageTitle }}
       </ToastTitle>
       <ToastDescription as-child>
-        hello
+        {{ notify.message }}
       </ToastDescription>
       <ToastAction
         class="[grid-area:_action]"
         as-child
         alt-text="Goto schedule to undo"
       >
-        <button class="inline-flex items-center justify-center rounded-md font-medium text-xs px-[10px] leading-[25px] h-[25px] bg-green2 text-green11 shadow-[inset_0_0_0_1px] shadow-green7 hover:shadow-[inset_0_0_0_1px] hover:shadow-green8 focus:shadow-[0_0_0_2px] focus:shadow-green8">
+        <button @click="open = false;" class="inline-flex items-center justify-center rounded-md font-medium text-xs px-[10px] leading-[25px] h-[25px] bg-green2 text-green11 shadow-[inset_0_0_0_1px] shadow-green7 hover:shadow-[inset_0_0_0_1px] hover:shadow-green8 focus:shadow-[0_0_0_2px] focus:shadow-green8">
           Close
         </button>
       </ToastAction>
