9c686a2db1d602c26d8802c461ab9a11e7984471
jolsefu <jules.leo.reserva@gmail.com>
2025-04-27 16:08:41 +0800

    Use reka-ui toast notifications

--- geartrac_frontend/src/components/Notify.vue
+++ geartrac_frontend/src/components/Notify.vue
@@ -1,20 +1,18 @@
 <script setup>
 import { ref, watch } from 'vue';
-import { Alert, AlertTitle, AlertDescription } from '@/components/ui/alert';
+import { ToastAction, ToastDescription, ToastProvider, ToastRoot, ToastTitle, ToastViewport } from 'reka-ui';
 
 const props = defineProps({
   notify: { type: Object, required: true },
 })
 
-const isVisible = ref(false);
+const open = ref(true);
 
 watch(
   props.notify,
   (newNotify) => {
-    console.log(newNotify)
-
     if (newNotify.message && newNotify.messageTitle) {
-      isVisible.value = true;
+      open.value = true;
       setTimeout(() => {
         isVisible.value = false;
       }, 7000);
@@ -25,24 +23,30 @@ watch(
 </script>
 
 <template>
-  <transition name="swipe-down">
-    <div
-      v-if="isVisible"
-      class="fixed top-4 left-1/2 transform -translate-x-1/2 w-sm text-white p-4 shadow-md transition-transform duration-500 rounded-md mt-4 z-10"
-      :class="{ 'translate-y-0': isVisible, '-translate-y-full': !isVisible }"
+  <ToastProvider>
+    <ToastRoot
+      v-model:open="open"
+      class="bg-white rounded-lg shadow-sm border p-[15px] grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--reka-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
     >
-      <Alert :variant="notify.messageTitle == 'Error' ? 'destructive' : ''" class="flex flex-col items-start">
-        <AlertTitle class="font-bold text-[1rem]">{{ notify.messageTitle }}</AlertTitle>
-        <AlertDescription>
-          {{ notify.message }}
-        </AlertDescription>
-      </Alert>
-    </div>
-  </transition>
+      <ToastTitle class="[grid-area:_title] mb-[5px] font-medium text-slate12 text-sm">
+        HELLO
+      </ToastTitle>
+      <ToastDescription as-child>
+        hello
+      </ToastDescription>
+      <ToastAction
+        class="[grid-area:_action]"
+        as-child
+        alt-text="Goto schedule to undo"
+      >
+        <button class="inline-flex items-center justify-center rounded-md font-medium text-xs px-[10px] leading-[25px] h-[25px] bg-green2 text-green11 shadow-[inset_0_0_0_1px] shadow-green7 hover:shadow-[inset_0_0_0_1px] hover:shadow-green8 focus:shadow-[0_0_0_2px] focus:shadow-green8">
+          Close
+        </button>
+      </ToastAction>
+    </ToastRoot>
+    <ToastViewport class="[--viewport-padding:_25px] fixed bottom-0 right-0 flex flex-col p-[var(--viewport-padding)] gap-[10px] w-[390px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none" />
+  </ToastProvider>
 </template>
 
 <style scoped>
-.fixed {
-  z-index: 1000;
-}
 </style>
