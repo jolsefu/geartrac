f5fe219ec55c3a1f71fd887e2a660979b1dae66d
jolsefu <jules.leo.reserva@gmail.com>
2025-04-29 11:09:05 +0800

    Added an animated welcome message for logged in users

--- geartrac_frontend/src/views/HomeView.vue
+++ geartrac_frontend/src/views/HomeView.vue
@@ -1,27 +1,47 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { isAuthenticated, userDetails } from "@/auth";
 
-const isVisible = ref(false);
+const isVisibleWelcome = ref(false);
+const isVisibleMain = ref(false);
 
-setTimeout(() => {
-  isVisible.value = true;
-}, 200);
+onMounted(() => {
+  if (isAuthenticated) {
+    setTimeout(() => {
+      isVisibleWelcome.value = true;
+    }, 300);
+
+    setTimeout(() => {
+      isVisibleWelcome.value = false;
+    }, 2000);
+
+    setTimeout(() => {
+      isVisibleMain.value = true;
+    }, 3000);
+  } else {
+    setTimeout(() => {
+      isVisibleMain.value = true;
+    }, 200);
+  }
+});
 </script>
 
 <template>
   <div class="flex items-center justify-center h-screen text-center flex-col">
     <Transition name="swipe-down">
-      <div v-if="isVisible">
-        <div v-if="isAuthenticated" class="mb-5">
-          <span>Welcome, {{ userDetails.first_name }}</span>
-        </div>
-        <h1 v-if="isVisible" class="text-8xl text-white font-bold">GearTRAC</h1>
+      <div v-if="isVisibleWelcome">
+        <h1 class="text-xl text-white font-bold">
+          Welcome, {{ userDetails.first_name }}
+        </h1>
       </div>
     </Transition>
 
+    <Transition name="swipe-down">
+      <h1 v-if="isVisibleMain" class="text-8xl text-white font-bold">GearTRAC</h1>
+    </Transition>
+
     <Transition name="swipe-up">
-      <div v-if="isVisible">
+      <div v-if="isVisibleMain">
         <h1 class="text-lg text-white mt-4">
           Developed Exclusively for The Gold Panicles
         </h1>
