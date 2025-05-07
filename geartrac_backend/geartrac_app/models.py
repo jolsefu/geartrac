d553a1be9a4ee07d901a41c7a427077b4fd0b0bd
jolsefu <jules.leo.reserva@gmail.com>
2025-05-07 19:56:30 +0800

    Add notifications

--- geartrac_backend/geartrac_app/models.py
+++ geartrac_backend/geartrac_app/models.py
@@ -134,3 +134,12 @@ class Log(models.Model):
 
     def __str__(self):
         return f'{self.user.email} - {self.action} - {self.timestamp}'
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"To {self.recipient}: {self.message[:20]}"
