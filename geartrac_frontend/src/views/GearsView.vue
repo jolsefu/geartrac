d6974638477934fdd0e6fe7c851b2e3b5f066b25
jolsefu <jules.leo.reserva@gmail.com>
2025-05-25 22:06:01 +0800

    Style changes and fixed return date picked bug

--- geartrac_frontend/src/views/GearsView.vue
+++ geartrac_frontend/src/views/GearsView.vue
@@ -4,18 +4,9 @@ import { api } from "@/api";
 import { userDetails } from "@/auth";
 import { Button } from "@/components/ui/button";
 import Pagination from "@/components/Pagination.vue";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import Notify from "@/components/Notify.vue";
 import "cally";
 
-const returnDatePicked = ref();
 const conditionBefore = ref();
 const isVisible = ref();
 const currentGear = reactive({});
@@ -27,6 +18,7 @@ const paginator = reactive({
   gearIds: [],
   available: null,
   request_user_owner: false,
+  returnDatePicked: null,
   search: null,
 
   pagesCount: 0,
@@ -177,13 +169,13 @@ function handleBorrow() {
 
     return;
   } else {
-    const modal = document.getElementById("borrow_modal");
+    const modal = document.getElementById("borrow-modal");
     modal.showModal();
   }
 }
 
 function borrowGear() {
-  const returnDate = new Date(returnDatePicked.value);
+  const returnDate = new Date(paginator.returnDatePicked);
   returnDate.setHours(23, 59, 0);
 
   api
@@ -209,6 +201,24 @@ function borrowGear() {
     });
 }
 
+function returnDateHandler(datetime) {
+  const returnDate = new Date(datetime);
+  const currentDate = new Date();
+  currentDate.setHours(0, 0, 0, 0);
+
+  if (returnDate <= currentDate) {
+    notify.message = "Return date must be a future date.";
+    notify.messageTitle = "Error";
+    notify.error = true;
+
+    document.querySelector("#borrow-modal").close();
+
+    return (paginator.returnDatePicked = null);
+  }
+
+  paginator.returnDatePicked = datetime;
+}
+
 function gearStatus(gear) {
   const fullName = `${userDetails.value.first_name} ${userDetails.value.last_name}`;
   if (gear.used_by === fullName) return "Used by You";
@@ -222,7 +232,7 @@ function gearCheckbox(gear) {
   const isUsedByUser = gear.used_by === fullName;
 
   if (useToggle.value) {
-    return isUsedByUser;
+    return gear.used || gear.borrowed || isUsedByUser;
   }
 
   if (unuseToggle.value) {
@@ -231,6 +241,11 @@ function gearCheckbox(gear) {
 
   if (isUsedByUser) return false;
   return gear.used || gear.borrowed;
+}
+
+function openGearModal(gear) {
+  Object.assign(currentGear, gear);
+  document.querySelector("#gear-modal").showModal();
 }
 
 onMounted(() => {
@@ -249,117 +264,115 @@ onMounted(() => {
       v-if="isVisible"
       class="flex items-center justify-center h-screen text-center flex-col"
     >
-      <Dialog>
-        <div class="flex items-center w-full h-screen justify-center">
-          <div class="flex justify-center gap-2 h-screen items-end w-1/4 flex-col">
-            <div>
-              <input
-                type="text"
-                placeholder="Enter Gear Name"
-                className="input"
-                v-model="paginator.search"
-              />
-            </div>
-
-            <div class="flex gap-1">
-              <Button
-                @click="paginator.request_user_owner = !paginator.request_user_owner"
-              >
-                {{ paginator.request_user_owner ? "Used by You" : "Used by Anyone" }}
-              </Button>
-              <Button @click="cycleAvailability">{{
-                paginator.available === null
-                  ? "All"
-                  : paginator.available
-                  ? "Available"
-                  : "Unavailable"
-              }}</Button>
-            </div>
+      <div class="flex items-center w-full h-screen justify-center">
+        <div class="flex justify-center gap-2 h-screen items-end w-1/4 flex-col">
+          <div>
+            <input
+              type="text"
+              placeholder="Enter Gear Name"
+              className="input"
+              v-model="paginator.search"
+            />
           </div>
 
-          <div class="flex items-center h-screen text-center w-1/2">
-            <div class="container mx-auto px-4 w-fit mt-2">
-              <div>
-                <Pagination
-                  v-if="paginator.gears.length"
-                  :total-items="paginator.pagesCount"
-                  :current-page="paginator.currentPage"
-                  :next="paginator.next"
-                  :previous="paginator.previous"
-                  class="mb-5"
-                  @update:current-page="(n) => (paginator.currentPage = n)"
-                  @update-page="getGears()"
-                />
-              </div>
-              <div v-if="paginator.gearIds.length" class="flex gap-1 justify-center mb-5">
-                <Button
-                  class="bg-green-500 text-black hover:bg-green-600"
-                  @click="useGear"
-                  v-if="useToggle"
-                >
-                  Use
-                </Button>
-                <Button
-                  class="bg-red-500 text-black hover:bg-red-600"
-                  @click="unuseGear"
-                  v-if="unuseToggle"
-                >
-                  Unuse
-                </Button>
-                <Button
-                  class="bg-blue-700 text-black hover:bg-blue-800"
-                  @click="handleBorrow"
-                >
-                  Borrow
-                </Button>
-              </div>
+          <div class="flex gap-1">
+            <Button @click="paginator.request_user_owner = !paginator.request_user_owner">
+              {{ paginator.request_user_owner ? "Used by You" : "Used by Anyone" }}
+            </Button>
+            <Button @click="cycleAvailability">{{
+              paginator.available === null
+                ? "All"
+                : paginator.available
+                ? "Available"
+                : "Unavailable"
+            }}</Button>
+          </div>
+        </div>
 
-              <div v-if="!paginator.gears.length">
-                <h3 class="text-2xl">No gears!</h3>
-              </div>
-              <div
-                v-for="gear in paginator.gears"
-                :key="gear.property_number"
-                class="mb-4 p-4 border rounded shadow flex"
+        <div class="flex items-center h-screen text-center w-1/2">
+          <div class="container mx-auto px-4 w-fit mt-2">
+            <div>
+              <Pagination
+                v-if="paginator.gears.length"
+                :total-items="paginator.pagesCount"
+                :current-page="paginator.currentPage"
+                :next="paginator.next"
+                :previous="paginator.previous"
+                class="mb-5"
+                @update:current-page="(n) => (paginator.currentPage = n)"
+                @update-page="getGears()"
+              />
+            </div>
+            <div v-if="paginator.gearIds.length" class="flex gap-1 justify-center mb-5">
+              <Button
+                class="bg-green-500 text-black hover:bg-green-600"
+                @click="useGear"
+                v-if="useToggle"
               >
-                <div class="flex items-center">
-                  <label>
-                    <input
-                      type="checkbox"
-                      :value="gear"
-                      @change="handleCheckboxChange(gear, $event)"
-                      :disabled="gearCheckbox(gear)"
-                    />
-                  </label>
-                </div>
+                Use
+              </Button>
+              <Button
+                class="bg-red-500 text-black hover:bg-red-600"
+                @click="unuseGear"
+                v-if="unuseToggle"
+              >
+                Unuse
+              </Button>
+              <Button
+                class="bg-blue-700 text-black hover:bg-blue-800"
+                @click="handleBorrow"
+              >
+                Borrow
+              </Button>
+            </div>
 
-                <div class="border-r border-gray-300 mx-4"></div>
-
-                <h2>
-                  <DialogTrigger as-child>
-                    <Button
-                      class="bg-white text-black hover:bg-[#cccccc] hover:text-black"
-                      @click="Object.assign(currentGear, gear)"
-                    >
-                      {{ gear.name }}
-                      <span class="text-[#4e4e4e]">
-                        {{ gearStatus(gear) }}
-                      </span>
-                    </Button>
-                  </DialogTrigger>
-                </h2>
+            <div v-if="!paginator.gears.length">
+              <h3 class="text-2xl">No gears!</h3>
+            </div>
+            <div
+              v-for="gear in paginator.gears"
+              :key="gear.property_number"
+              class="mb-4 p-4 border rounded shadow flex"
+            >
+              <div class="flex items-center">
+                <label>
+                  <input
+                    type="checkbox"
+                    :value="gear"
+                    @change="handleCheckboxChange(gear, $event)"
+                    :disabled="gearCheckbox(gear)"
+                    :checked="paginator.gearIds.includes(gear.id)"
+                  />
+                </label>
               </div>
+
+              <div class="border-r border-gray-300 mx-4"></div>
+
+              <h2>
+                <Button
+                  class="bg-white text-black hover:bg-[#cccccc] hover:text-black"
+                  @click="openGearModal(gear)"
+                >
+                  {{ gear.name }}
+                  <span class="text-[#4e4e4e]">
+                    {{ gearStatus(gear) }}
+                  </span>
+                </Button>
+              </h2>
             </div>
           </div>
         </div>
 
-        <DialogContent class="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>
-              {{ currentGear.name }}
-              {{ currentGear.property_number }}
-            </DialogTitle>
-            <DialogDescription>
+        <dialog id="gear-modal" class="modal">
+          <div
+            class="modal-box text-left border-2 border-neutral-500 rounded-lg text-white w-fit"
+          >
+            <div>
+              <h3 class="text-lg font-bold">
+                {{ currentGear.name }}
+                {{ currentGear.property_number }}
+              </h3>
+
               <div v-if="!currentGear.used && !currentGear.borrowed" class="text-white">
                 This gear is available.
               </div>
@@ -370,53 +383,27 @@ onMounted(() => {
               <div v-if="currentGear.borrowed_by">
                 Borrowed by: {{ currentGear.borrowed_by }}
               </div>
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
+            </div>
+            <div class="modal-action justify-start">
+              <form method="dialog">
+                <button class="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      </div>
 
-      <dialog id="borrow_modal" class="modal">
+      <dialog id="borrow-modal" class="modal">
         <div class="modal-box">
           <div class="font-bold">Borrower's Slip Details</div>
-          <div class="modal-action">
-            <button
-              popoverTarget="cally-popover1"
-              className="input input-border"
-              id="cally1"
-              style="anchorname: --cally1"
-            >
-              {{ returnDatePicked ? returnDatePicked : "Expected Return Date" }}
-            </button>
-            <div
-              popover
-              id="cally-popover1"
-              className="dropdown bg-base-100 rounded-box shadow-lg"
-              style="positionanchor: --cally1"
-            >
-              <calendar-date
-                class="cally"
-                :onchange="(e) => (returnDatePicked = e.target.value)"
-              >
-                <svg
-                  aria-label="Previous"
-                  className="fill-current size-4"
-                  slot="previous"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M15.75 19.5 8.25 12l7.5-7.5"></path>
-                </svg>
-                <svg
-                  aria-label="Next"
-                  className="fill-current size-4"
-                  slot="next"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="m8.25 4.5 7.5 7.5-7.5 7.5"></path>
-                </svg>
-                <calendar-month></calendar-month>
-              </calendar-date>
+          <div class="modal-action flex justify-center">
+            <div>
+              <input
+                type="date"
+                class="input"
+                v-model="paginator.returnDatePicked"
+                @change="(e) => returnDateHandler(e.target.value)"
+              />
             </div>
 
             <button
